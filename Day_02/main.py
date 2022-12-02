9d38f866c4adb73cda30ab93de46b07cab600ba3
Maximilian Schmidt <darkblade4k@googlemail.com>
2022-12-02 06:32:42 +0100

    refatored for part 2

--- Day_02/main.py
+++ Day_02/main.py
@@ -3,48 +3,55 @@ with open("input.prod", "r") as f:
 
 # for line in lines:
     # print(line)
+def main_01(data):
+    plays = {
+        "A": "Rock",
+        "B": "Paper",
+        "C": "Scissors",
 
-plays = {
-    "A": "Rock",
-    "B": "Paper",
-    "C": "Scissors",
+        "X": "Rock",
+        "Y": "Paper",
+        "Z": "Scissors",
+    }
 
-    "X": "Rock",
-    "Y": "Paper",
-    "Z": "Scissors",
-}
+    scores = {
+        "X": 1,
+        "Y": 2,
+        "Z": 3,
+    }
 
-scores = {
-    "X": 1,
-    "Y": 2,
-    "Z": 3,
-}
+    score = 0
+    for line in data:
+        enemy_play = line[0]
+        player_play = line[2]
 
-score = 0
-for line in lines:
-    enemy_play = line[0]
-    player_play = line[2]
+        if plays[enemy_play] == plays[player_play]:
+            score += scores[player_play]
+            score += 3
+        elif plays[enemy_play] == "Rock" and plays[player_play] == "Paper":
+            score += scores[player_play]
+            score += 6
+        elif plays[enemy_play] == "Rock" and plays[player_play] == "Scissors":
+            score += scores[player_play]
+            score += 0
+        elif plays[enemy_play] == "Paper" and plays[player_play] == "Rock":
+            score += scores[player_play]
+            score += 0
+        elif plays[enemy_play] == "Paper" and plays[player_play] == "Scissors":
+            score += scores[player_play]
+            score += 6
+        elif plays[enemy_play] == "Scissors" and plays[player_play] == "Rock":
+            score += scores[player_play]
+            score += 6
+        elif plays[enemy_play] == "Scissors" and plays[player_play] == "Paper":
+            score += scores[player_play]
+            score += 0
 
-    if plays[enemy_play] == plays[player_play]:
-        score += scores[player_play]
-        score += 3
-    elif plays[enemy_play] == "Rock" and plays[player_play] == "Paper":
-        score += scores[player_play]
-        score += 6
-    elif plays[enemy_play] == "Rock" and plays[player_play] == "Scissors":
-        score += scores[player_play]
-        score += 0
-    elif plays[enemy_play] == "Paper" and plays[player_play] == "Rock":
-        score += scores[player_play]
-        score += 0
-    elif plays[enemy_play] == "Paper" and plays[player_play] == "Scissors":
-        score += scores[player_play]
-        score += 6
-    elif plays[enemy_play] == "Scissors" and plays[player_play] == "Rock":
-        score += scores[player_play]
-        score += 6
-    elif plays[enemy_play] == "Scissors" and plays[player_play] == "Paper":
-        score += scores[player_play]
-        score += 0
+    print("Score for first Answer is: ", score)
 
-print("Score for first Answer is: ", score)
+
+def main_02(data):
+
+
+if __name__ == "__main__":
+    main_01(lines)
