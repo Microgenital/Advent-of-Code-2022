6f88991703890bc2c05294a6a792be1f6ca0ccec
Maximilian Schmidt <darkblade4k@googlemail.com>
2022-12-03 09:27:11 +0100

    started part 2

--- Day_03/main.py
+++ Day_03/main.py
@@ -1,4 +1,4 @@
-with open("input.prod", "r") as f:
+with open("input.test", "r") as f:
     lines = f.readlines()
 
 def main_01(data):
@@ -74,5 +74,77 @@ def main_01(data):
     print(score)
 
 
+def main_02(data):
+    score = 0
+    data_index = 0
+
+    priority = {
+        "a": 1,
+        "b": 2,
+        "c": 3,
+        "d": 4,
+        "e": 5,
+        "f": 6,
+        "g": 7,
+        "h": 8,
+        "i": 9,
+        "j": 10,
+        "k": 11,
+        "l": 12,
+        "m": 13,
+        "n": 14,
+        "o": 15,
+        "p": 16,
+        "q": 17,
+        "r": 18,
+        "s": 19,
+        "t": 20,
+        "u": 21,
+        "v": 22,
+        "w": 23,
+        "x": 24,
+        "y": 25,
+        "z": 26,
+        "A": 27,
+        "B": 28,
+        "C": 29,
+        "D": 30,
+        "E": 31,
+        "F": 32,
+        "G": 33,
+        "H": 34,
+        "I": 35,
+        "J": 36,
+        "K": 37,
+        "L": 38,
+        "M": 39,
+        "N": 40,
+        "O": 41,
+        "P": 42,
+        "Q": 43,
+        "R": 44,
+        "S": 45,
+        "T": 46,
+        "U": 47,
+        "V": 48,
+        "W": 49,
+        "X": 50,
+        "Y": 51,
+        "Z": 52,
+    }
+
+    for i in data:
+        for j in priority:
+            if j in data[data_index] and j in data[data_index + 1] and j in data[data_index + 2]:
+                score += priority[j]
+            data_index += 3
+            if data_index >= len(data):
+                break
+        data_index = 0
+
+
+
+    print(score)
+
 if __name__ == "__main__":
-    main_01(lines)
+    main_02(lines)
