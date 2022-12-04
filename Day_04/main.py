5ea906c15201cfde031d9912f4cd3948c8be76f3
Maximilian Schmidt <darkblade4k@googlemail.com>
2022-12-04 09:17:24 +0100

    started solution 01

--- Day_04/main.py
+++ Day_04/main.py
@@ -2,7 +2,19 @@ with open("input.test") as f:
     data = f.read().splitlines()
 
 def main_01(data):
-    pass
+    data2 = []
+    for line in data:
+        data2.append(line.split(","))
+
+
+    for i in data2:
+        first_num = int(i[0][0])
+        second_num = int(i[0][2])
+        for num in range(first_num, second_num + 1):
+            print(num)
+        print("\n")
+
+
 
 def main_02(data):
     pass
