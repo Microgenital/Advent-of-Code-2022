a22081b4b667521f284b88f4e37df3f197a17924
Max Schmidt <darkblade4k@gmail.com>
2022-12-05 07:56:50 +0100

    prepared files

---
+++ Day_05/main.py
@@ -0,0 +1,12 @@
+with open("input.test") as f:
+    data = f.read().splitlines()
+
+def main_01(data):
+    pass
+
+def main_02(data):
+    pass
+
+if __name__ == "__main__":
+    print(main_01(data))
+    print(main_02(data))
