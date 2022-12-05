ecc8b5ed90c126942adcfa7215619de75e59f810
Max Schmidt <DarkBlade4k@gmail.com>
2022-12-05 13:07:35 +0100

    solved part 1

--- Day_05/main.py
+++ Day_05/main.py
@@ -1,4 +1,4 @@
-with open("input.test") as f:
+with open("input.prod") as f:
     data = f.read().splitlines()
 
 
@@ -6,15 +6,57 @@ def main_01(data):
     # sort instructions and creates
     creates = []
     instructions = []
+
+    # sort input in creates and instructions
     for line in data:
         if "move" in line:
             instructions.append(line)
         else:
-            creates.append(line)
+            continue
 
-    # print as 2d array
-    for i in range(len(creates)):
-        print(creates[i])
+    creates_test = [
+        [],
+        ["Z", "N"],
+        ["M", "C", "D"],
+        ["P"]
+    ]
+
+    """
+    Had to manually enter the creates_list, because I couldn't figure out how to sort it automatically.
+    """
+    creates = [
+        [],
+        ["V", "C", "D", "R", "Z", "G", "B", "W"],
+        ["G", "W", "F", "C", "B", "W", "T", "V"],
+        ["C", "B", "S", "N", "W"],
+        ["Q", "G", "M", "N", "J", "V", "C", "P"],
+        ["T", "S", "L", "F", "D", "H", "B"],
+        ["J", "V", "T", "W", "M", "N"],
+        ["P", "F", "L", "C", "S", "T", "G"],
+        ["B", "D", "Z"],
+        ["M", "N", "Z", "W"]
+    ]
+
+    # make list for all instructions readable
+    lines_clr = []
+    for i in instructions:
+        lines_clr.append(i.split(" "))
+
+    # execute instructions
+    for i in lines_clr:
+        count = i[1]
+        from_ = i[3]
+        to_ = i[5]
+        for c in range(int(count)):
+            creates[int(to_)].append(creates[int(from_)].pop())
+
+    # create and print solution
+    solution = ""
+    for i in creates:
+        if len(i) == 0:
+            continue
+        solution += i[-1]
+    print(solution)
 
 
 def main_02(data):
@@ -22,5 +64,5 @@ def main_02(data):
 
 
 if __name__ == "__main__":
-    print(main_01(data))
-    print(main_02(data))
+    main_01(data)
+    # main_02(data)
