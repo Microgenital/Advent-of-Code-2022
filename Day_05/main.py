56ccb08d5109027c542d8fa94be262b7a2de0e68
Max Schmidt <darkblade4k@gmail.com>
2022-12-05 08:05:29 +0100

    started solution 1

--- Day_05/main.py
+++ Day_05/main.py
@@ -1,11 +1,25 @@
 with open("input.test") as f:
     data = f.read().splitlines()
 
+
 def main_01(data):
-    pass
+    # sort instructions and creates
+    creates = []
+    instructions = []
+    for line in data:
+        if "move" in line:
+            instructions.append(line)
+        else:
+            creates.append(line)
+
+    # print as 2d array
+    for i in range(len(creates)):
+        print(creates[i])
+
 
 def main_02(data):
     pass
+
 
 if __name__ == "__main__":
     print(main_01(data))
