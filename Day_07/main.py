e064469297288122660321276d0cdf479a77becd
Max Schmidt <darkblade4k@gmail.com>
2022-12-07 20:53:36 +0100

    started the solution

--- Day_07/main.py
+++ Day_07/main.py
@@ -4,11 +4,33 @@ with open("input.test", "r") as f:
 
 
 def main_01(data):
-    pass
+    # data input is linux commands
+    # go ttrough each command and execute it
+    # return the value of the register a
+
+    instructions = []
+    for line in data:
+        instructions.append(line.split(" "))
+
+    for i in instructions:
+        if i[0] == "$":
+            print(i[1])
+
+    """path = []
+    for line in data:
+        if "$" in line: # line is entered by user
+            if "cd" in line and ".." in line:
+                pass
+            elif "cd" in line:
+                pass
+            elif "ls" in line:
+                pass"""
+
+
 
 def main_02(data):
     pass
 
 if __name__ == "__main__":
-    main_01()
-    main_02()
+    main_01(data)
+    main_02(data)
